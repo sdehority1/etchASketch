19bae3dba67a043364fd6d46a8ac2d97ad280bcb
Sam <samdehority@outlook.com>
2025-01-04 20:42:28 -0500

    First update

---
+++ LOGIC.js
@@ -0,0 +1,11 @@
+blackDivsButton.addEventListener('click', function () {
+  let div = document.createElement("div");
+  div.appendChild(para);
+  document.getElementById("container").appendChild(div);
+  alert("it's working")
+
+});
+
+rainbowDivsButton.addEventListener('click', function () {
+  //alert ("it's working");
+});
