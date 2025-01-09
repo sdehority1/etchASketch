2dd6f508b14f53d38291c82048745615c85a2793
Sam <samdehority@outlook.com>
2025-01-08 20:56:40 -0500

    Trying to figure otu the math to generate the divs to fit in parent container. Added a button to clear the divs that have been added. Added parameters to ensure only a certain number of divs allowed.

--- LOGIC.js
+++ LOGIC.js
@@ -1,15 +1,37 @@
 blackDivsButton.addEventListener('click', function () {
-  let i = document.getElementById('blackDivs').value;
-  alert(typeof i);
-  i= Number(i);
-  alert(typeof i);
+  let inputRaw = document.getElementById('blackDivs');
+  let i = inputRaw.value;
+  let j = 1;
+
+  if (i < 1 || i > 99) {
+    inputRaw.value = ""; // Clear the input field
+    i = 0;
+    alert("Please enter a number between 1 and 99");
+  } else {
+    i = Number(i);
+  }
+
+  for (let j = 1; j <= i; j++) {
+    let div = document.createElement("div");
+    div.textContent = 'Yes';
+    div.className = "blackStyledDiv";
+    document.getElementById("container").appendChild(div);
+  }
+
+  //alert(typeof i);
+  //alert(i);
   //let div = document.createElement("div");
   //div.appendChild(para);
   //document.getElementById("container").appendChild(div);
-
-
 });
 
 rainbowDivsButton.addEventListener('click', function () {
-  alert ("it's working");
+  alert("it's working");
+});
+
+clearButton.addEventListener('click', function () {
+  //HOW THIS WORKS: While container has a child, RUN this code. removeChild does exactly that, removes the child element ONLY IF it has a first child. 
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 });
